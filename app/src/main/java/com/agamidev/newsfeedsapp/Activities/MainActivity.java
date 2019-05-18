e4cb4069f803c94e684a43fff50813cc543154cd
MuhammadNagi <mohamed.elagami85@gmail.com>
2019-05-18 17:38:28 +0200

    Fifth Commit

--- app/src/main/java/com/agamidev/newsfeedsapp/Activities/MainActivity.java
+++ app/src/main/java/com/agamidev/newsfeedsapp/Activities/MainActivity.java
@@ -2,9 +2,14 @@ package com.agamidev.newsfeedsapp.Activities;
 
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -12,6 +17,8 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
@@ -114,8 +121,6 @@ public class MainActivity extends AppCompatActivity implements MainContract.Main
 
         this.addContentView(relativeLayout, params);
     }
-
-
 
 
 
